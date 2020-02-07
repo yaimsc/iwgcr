809787eaeeebb7a2d59409404423193bf1bbd4a6
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-07 15:05:52 +0100

    add card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,9 +4,10 @@
 
 @section('content')
 
+<div class="index">
 <img src="/img/salto.png" id="img-salto"/>
-  <div id="form-centre">
-    <h5>Centre Information<h5>
+  <div id="form-centre" class="card">
+    <h5 class="title">Centre Information<h5>
     <form method="post">
       @csrf
       <div class="form-group">
@@ -47,17 +48,28 @@
 
     </form>
   </div>
-  <div id="form-centre">
-    <h5>Centre Staff Contact Person<h5>
+  <div id="form-staff" class="card">
+    <h5 class="title">Centre Staff Contact Person<h5>
     <form method="post">
       @csrf
       <div class="form-group">
         <label class="bmd-label-floating">Name</label>
          <input type="text" class="form-control" name="name" id="name" min="2" max="35" required/> 
       </div>
-      <div class="form-group">
-        {{-- <label>Telephone Number</label> --}}
-        <input type="tel" class="form-control" name="number" placeholder="+ 34 671 29 05 34" required/>
+      <div class="form-row">
+    
+        <div class="form-group col-md-6">
+          <select class="form-control" name="telephonecode" id="telephonecode">
+            <option value="" hidden disabled selected class="placeholder">+ 34</option>
+              @foreach ($countries as $country)
+                <option value="{{$country->telephonecode}}">{{$country->name}} {{$country->telephonecode}}</option>
+              @endforeach
+          </select>
+        </div>
+        <div class="form-group col-md-6">
+          <input type="tel" id="telephone" class="form-control" name="telephonenumber" placeholder="671 29 05 34" required/>
+        </div>
+        
       </div>
       {{-- <div class="form-row"> --}}
         {{-- <div id="app">
@@ -87,7 +99,7 @@
       </div>
     </form>
   </div>
-
+</div>
 {{-- <script type="text/javascript" src="{{asset('./js/app.js')}}"></script> --}}
   
   
