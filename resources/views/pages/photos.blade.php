df8d1fe490bee58d8cd352d94eba7aa7fd560145
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-12 18:02:05 +0100

    photos

--- resources/views/pages/photos.blade.php
+++ resources/views/pages/photos.blade.php
@@ -3,6 +3,17 @@
 @section('title', 'Regus Survey')
 
 @section('content')
+
+<div class="photos">
+  <div class="card">
+    <div class="card-header">
+      <h5 class="title">Interior Photo</h5>
+    </div>
+    <div>
+      <input type="file" />
+    </div>
+  </div>
+</div>
     
 @endsection
     
