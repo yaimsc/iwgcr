40fb5dd35cf0ab3434f4fa8b6c694b4fa16872a7
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-12 12:27:44 +0100

    form

--- resources/views/pages/pdf.blade.php
+++ resources/views/pages/pdf.blade.php
@@ -4,9 +4,11 @@
 
 @section('content')
 
+<div id="pdf">
   <h3>Download the PDF and follow the details</h3>
-  <div class="col-lg-2 mx-auto">
-      <a href="/download/REGUS_Survey_Comms_Rooms.pdf"><img id="pdf" src="img/icon_pdf.png" alt="Regus Survey Requirements" ></a>
-  </div>
+</div>  
+<div class="col-lg-2 mx-auto">
+  <a href="/download/REGUS_Survey_Comms_Rooms.pdf"><img id="pdf" src="img/icon_pdf.png" alt="Regus Survey Requirements"></a>
+</div>
     
 @endsection
