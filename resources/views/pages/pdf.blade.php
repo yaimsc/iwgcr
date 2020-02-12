468ff13697a58241c6df7d63e9438b61e80741be
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-12 15:52:22 +0100

    pdf page

--- resources/views/pages/pdf.blade.php
+++ resources/views/pages/pdf.blade.php
@@ -4,11 +4,18 @@
 
 @section('content')
 
-<div id="pdf">
-  <h3>Download the PDF and follow the details</h3>
-</div>  
-<div class="col-lg-2 mx-auto">
-  <a href="/download/REGUS_Survey_Comms_Rooms.pdf"><img id="pdf" src="img/icon_pdf.png" alt="Regus Survey Requirements"></a>
+<div id="pdf-content">
+  <div id="pdf-title">
+    <h3>Download the PDF and follow the details</h3>
+  </div>
+  <div id="pdf-icon">
+    <a href="/download/REGUS_Survey_Comms_Rooms.pdf"><img id="pdf" src="img/icon_pdf.png" alt="Regus Survey Requirements"></a>
+  </div>
+  <div id="pdf-button">
+    <button class="btn-light">
+      {{ __('NEXT') }}
+    </button>
+  </div>
 </div>
     
 @endsection
