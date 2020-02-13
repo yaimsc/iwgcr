f4842e674a152d2996a63fe5d65c9e3cd7a529b6
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-13 15:32:33 +0100

    edit fisr forms

--- resources/views/pages/centre.blade.php
+++ resources/views/pages/centre.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.form')
 @section('title', 'Regus Survey - Centre information')
 @section('content')
-<div id="form-centre" class="card-contact">
+<div id="card-contact" class="card">
   <div class="card-header">
     <h5 class="title">Centre Information<h5>
   </div>
--- resources/views/pages/photos.blade.php
+++ resources/views/pages/photos.blade.php
@@ -5,40 +5,49 @@
 @section('content')
 
 <div class="photos-content">
-  <div id="photo">
-    <div class="card">
-      <label class="title">Interior Photo</label>
-      <input class="form-control"  type="file" name="interior"/>
-    </div>
-    <div class="card">
-      <label class="title">Front Photo</label>
-      <input class="form-control" type="file" name="front"/>
-    </div>
-    <div class="card">
-      <label class="title">Exterior Photo</label>
-      <input class="form-control" type="file" name="exterior"/>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header">
-      <h5 class="title">Cylinder Specifications</h5>
-    </div>
-    <div class="form-group">
-      <label class="bmd-label-floating">Cylinder Name</label>
-      <input class="form-control" name="name" placeholder="Cylinder Name"/>
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label class="bmd-label-floating">Exterior Length</label>
-        <input class="form-control" name="name" placeholder="Exterior Length (mm)"/>
+  <form>
+    @csrf
+    <div class="form">
+    <div id="photo">
+      <div class="card">
+        <label class="title">Interior Photo</label>
+        <input class="form-control"  type="file" name="interior"/>
       </div>
-      <div class="form-group col-md-6">
-        <label class="bmd-label-floating">Interior Length</label>
-        <input class="form-control" name="name" placeholder="Interior Length (mm)"/>
+      <div class="card">
+        <label class="title">Front Photo</label>
+        <input class="form-control" type="file" name="front"/>
+      </div>
+      <div class="card">
+        <label class="title">Exterior Photo</label>
+        <input class="form-control" type="file" name="exterior"/>
       </div>
     </div>
-   
+    <div class="card">
+      <div class="card-header">
+        <h5 class="title">Cylinder Specifications</h5>
+      </div>
+      <div class="form-group">
+        <label class="bmd-label-floating">Cylinder Name</label>
+        <input class="form-control" name="name" placeholder="Cylinder Name"/>
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label class="bmd-label-floating">Exterior Length</label>
+          <input class="form-control" name="name" placeholder="Exterior Length (mm)"/>
+        </div>
+        <div class="form-group col-md-6">
+          <label class="bmd-label-floating">Interior Length</label>
+          <input class="form-control" name="name" placeholder="Interior Length (mm)"/>
+        </div>
+      </div>
+    </div>
   </div>
+    <div class="submit">
+      <button type="submit" class="btn-light">
+        {{ __('SUBMIT') }}
+    </button>
+    </div>
+  </form>
 </div>
     
 @endsection
