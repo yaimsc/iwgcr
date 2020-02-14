15ad8a1814ec60e9db5a718f3e384362cca290e5
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-14 15:09:56 +0100

    starting drawer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.home')
 @section('title', 'Regus Survey Data')
 @section('content')
+  <div class="content">
+    <div class="drawer">
+      <div class="centres">
+        <a>
+          <i></i>
+          <div></div>
+        </a>
+      </div>
+      <div class="contacts">
+
+      </div>
+    </div>
+  </div>
 @endsection
