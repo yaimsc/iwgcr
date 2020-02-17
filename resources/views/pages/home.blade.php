c9dfbf446b301105d13841761492afcbcdfaae88
Yaiza Muñoz <y.munoz@LPHQ0203.saltosystems.com>
2020-02-17 11:51:34 +0100

    drawer style

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,15 +3,20 @@
 @section('content')
   <div class="content">
     <div class="drawer">
-      <div class="centres">
-        <a>
-          <i></i>
-          <div></div>
-        </a>
-      </div>
-      <div class="contacts">
-
-      </div>
+      <ul>
+        <li class="centres">
+          <a>
+            <i class="fas fa-building"></i>
+            <p>Centres</p>
+          </a>
+        </li>
+        <li class="contacts">
+          <a>
+            <i class="fas fa-users"></i>
+            <p>Users</p>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 @endsection
