a559434521f668afa626beea91b828d63e189a54
Yaiza Muñoz <y.munoz@LPHQ0203.local>
2020-04-20 18:07:44 +0200

    Mail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'webistes@saltosystems.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'websites@saltosystems.com'),
         'name' => env('MAIL_FROM_NAME', 'Salto Systems'),
     ],
 
