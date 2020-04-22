70f82afa891f39013d76bc4b869a8d61da2f976c
Yaiza Muñoz <y.munoz@LPHQ0203.local>
2020-04-22 16:39:59 +0200

    mail

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'websites@saltosystems.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', 'xvzwhnhovujbqczi'),
 
     /*
     |--------------------------------------------------------------------------
