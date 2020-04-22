c39fd6ca582c3d431bf06dabec44386b46a3a6be
Yaiza Muñoz <y.munoz@LPHQ0203.local>
2020-04-22 14:22:41 +0200

    commit

--- resources/views/pages/contactPerson.blade.php
+++ resources/views/pages/contactPerson.blade.php
@@ -69,7 +69,6 @@
             @enderror
           </div>
         </div>
-      </div>
       <div class="form-group">
         <label class="bmd-label-floating">Email</label>
         <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required/>
@@ -80,7 +79,7 @@
         @enderror
       </div>
       <div class="letra">
-      <p>The controller of the personal data provided in this form is Salto Systems, S.L. (“Salto”), who will process it in order to communicate with your Centre for providing the products and/or services requested as well as process and meet your request of receiving a quote for the products and services commercialized by Salto. You can exercise through the email privacy@saltosystems.com your rights of access, rectification, erasure, restriction, objection, data portability and not to be subject to a decision based solely on automated processing. Your data will not be used for profiling. <a href="{{route('privacy')}}"> Please click here to find additional Information on Data Protection.  </a></p>
+        <p>The controller of the personal data provided in this form is Salto Systems, S.L. (“Salto”), who will process it in order to communicate with your Centre for providing the products and/or services requested as well as process and meet your request of receiving a quote for the products and services commercialized by Salto. You can exercise through the email privacy@saltosystems.com your rights of access, rectification, erasure, restriction, objection, data portability and not to be subject to a decision based solely on automated processing. Your data will not be used for profiling. <a href="{{route('privacy')}}"> Please click here to find additional Information on Data Protection.  </a></p>
       </div>
     </div>
       <select class="form-control" name="centre_name" id="centre" hidden>
